5badcfc5052799a11939ca9cbe5d582058390f93
sushant-wayal <sushantssr12345@gmail.com>
2025-01-15 23:59:08 +0530

    Remove constants file to streamline configuration and improve code clarity

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const WEBSITE_NAME = "Github Readme Generator";
-export const AUTHOR_GITHUB_LINK = "https://github.com/sushant-wayal";
-export const GEMINI_AI_MODEL = "gemini-1.5-pro";
-export const ACCESS_TOKEN = "access_token";
-export const PER_PAGE = 10;
