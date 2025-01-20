29a52f964ced89883f70f6757988db908f40c35b
sushant-wayal <sushantssr12345@gmail.com>
2025-01-20 20:54:49 +0530

    Remove constants file from the repository and update .gitignore to improve code clarity and streamline configuration

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const NEXT_PUBLIC_PUSHER_KEY = "03bd61efaa0a2e65b974"
+export const NEXT_PUBLIC_PUSHER_CLUSTER = "ap2"
+
+export const WEBSITE_NAME = "Github Readme Generator";
+export const AUTHOR_GITHUB_LINK = "https://github.com/sushant-wayal";
+export const GEMINI_AI_MODEL = "gemini-1.5-pro";
+export const ACCESS_TOKEN = "access_token";
+export const PER_PAGE = 10;
+export const DEV_WEBHOOK_DOMAIN = "https://4298-2409-40c2-8046-8167-9020-dfcd-9463-8cca.ngrok-free.app";
+export const PROD_WEBHOOK_DOMAIN = "https://github-readme-generator-umber.vercel.app";
+export const COMMIT_EVENT = "commit-event";
+export const MARKDOWN_EVENT = "markdown-event";
+export const INITIAL_COMMIT_MSG = "Initial Generation";
